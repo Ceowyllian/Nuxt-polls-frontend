c11627be4ed6dced7f08f222884797d101956cee
Ceowyllian <downwell@online.ua>
2022-10-04 10:32:21 +0300

    Account page stub

---
+++ pages/account.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    <v-card-text>
+      Account page
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AccountPage'
+}
+</script>
+
+<style scoped>
+
+</style>
