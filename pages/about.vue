ccaf445ef05813346cdd7b9fe1808c1543c6dc13
Ceowyllian <downwell@online.ua>
2022-10-04 10:32:07 +0300

    About page stub

---
+++ pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-card>
+    <v-card-text>
+      About page
+    </v-card-text>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'AboutPage'
+}
+</script>
+
+<style scoped>
+
+</style>
